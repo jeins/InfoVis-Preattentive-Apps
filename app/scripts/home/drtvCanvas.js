349cf65f593ee60973517b3b2c509c4abcfb187d
jeins <mail@mjuanakbar.info>
2016-05-16 12:44:20 +0200

    draw shape on canvas, generate random answer of shapes

---
+++ app/scripts/home/drtvCanvas.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('infoVisU2App')
+    .directive('drtvCanvas', drtvCanvas);
+
+drtvCanvas.$inject = ['$timeout'];
+function drtvCanvas($timeout){
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.ready(function () {
+                var height,
+                    width;
+                $timeout(function () {
+                    height = element[0].offsetHeight;
+                    width = element[0].offsetWidth;
+                    if (attrs.key) {
+                        scope[attrs.key] = {
+                            height: height,
+                            width: width
+                        };
+                        return;
+                    }
+                    scope.$broadcast("canvas-size", [width, height]);
+                });
+            });
+        }
+    }
+}
--- app/scripts/preattentive_color/time_trial/CtrlColorTimeTrial.js
+++ app/scripts/preattentive_color/time_trial/CtrlColorTimeTrial.js
@@ -11,8 +11,44 @@ function CtrlColorTimeTrial($scope, $log){
     init();
 
     function init(){
-        $scope.$on('pressed-key', function(event, result){
-           $log.info("Key Pressed: " + result);
+        self.distance = 25;
+        $scope.$on('canvas-size', function (event, result) {
+            self.maxPosX = result[0]-(self.distance*2);
+            self.maxPosY = result[1]-(self.distance*2);
+            self.shapePosition = generateShape();
         });
+        
+        $scope.$on('pressed-key', function(event, result){
+            $log.info("Key Pressed: " + result + " " +self.answer);
+            if((result == "right" && self.answer == "present") || (result == "left" && self.answer == "absent")){
+                $log.info("Answer Correct!")
+            } else {
+                $log.info("Answer Wrong!")
+            }
+        });
+    }
+    
+    function generateShape(){
+        var shapePosition = [];
+        var randomX = Math.ceil(Math.random() * (self.maxPosX/self.distance)) * self.distance;
+        var randomY = Math.ceil(Math.random() * (self.maxPosY/self.distance)) * self.distance;
+        var randomAnswer = Math.ceil(Math.random() * 2); console.log(randomAnswer);
+        for(var i=0; i<self.maxPosX; i+=self.distance){
+            for(var j=0; j<self.maxPosY; j+=self.distance){
+                if(randomAnswer == 1){
+                    if(i == randomX && j == randomY) {
+                        shapePosition.push({"shape": "circle", "x": i, "y": j, "color": "red"});
+                        self.answer = "present";
+                    }
+                    else {
+                        shapePosition.push({"shape": "circle", "x": i, "y": j, "color": "green"});
+                    }
+                } else{
+                    shapePosition.push({"shape": "circle", "x": i, "y": j, "color": "green"});
+                    self.answer = "absent";
+                }
+            }
+        }
+        return shapePosition;
     }
 }
