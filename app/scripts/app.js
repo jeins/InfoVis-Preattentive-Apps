09d122c9585e9c6d98af869b5f01ac973a1ef211
jeins <mail@mjuanakbar.info>
2016-05-17 20:58:07 +0200

    setup log provider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,7 +12,23 @@ angular
   .module('infoVisU2App', [
     'ngAnimate', 'ngAria', 'ngRoute', 'ngMaterial'
   ])
-  .config(function ($routeProvider) {
+  .config(['$routeProvider', '$logProvider', '$provide', function ($routeProvider, $logProvider, $provide) {
+      // Setup Logging/Debug
+      $logProvider.debugEnabled(false);
+
+      $provide.decorator('$log', function ($delegate) {
+          var origInfo = $delegate.info, origLog = $delegate.log,
+              origError = $delegate.error, origWarn = $delegate.warn;
+
+          $delegate.info = function () {if ($logProvider.debugEnabled()) origInfo.apply(null, arguments)};
+          $delegate.log = function () {if ($logProvider.debugEnabled()) origLog.apply(null, arguments)};
+          $delegate.error = function () {if ($logProvider.debugEnabled()) origError.apply(null, arguments)};
+          $delegate.warn = function () {if ($logProvider.debugEnabled()) origWarn.apply(null, arguments)};
+
+          return $delegate;
+      });
+      
+      // Route
       $routeProvider
           .when('/', {
               templateUrl: 'views/home/home.html',
@@ -52,4 +68,4 @@ angular
           .otherwise({
               redirectTo: '/'
           });
-  });
+  }]);
