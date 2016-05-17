ce67d0c335d68ddd7358ebdd6729dd7be59f7950
jeins <mail@mjuanakbar.info>
2016-05-17 20:53:36 +0200

    refactor file order and finishing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,37 +15,37 @@ angular
   .config(function ($routeProvider) {
       $routeProvider
           .when('/', {
-              templateUrl: 'scripts/home/home.html',
+              templateUrl: 'views/home/home.html',
               controller: 'CtrlHome',
               controllerAs: 'vm'
           })
           .when('/colors/time-trial', {
-              templateUrl: 'scripts/preattentive_color/time_trial/view.html',
+              templateUrl: 'views/preattentive_color/time_trial.html',
               controller: 'CtrlColorTimeTrial',
               controllerAs: 'vm'
           })
           .when('/colors/short-visible', {
-              templateUrl: 'scripts/preattentive_color/short_visible/view.html',
+              templateUrl: 'views/preattentive_color/short_visible.html',
               controller: 'CtrlColorShortVisible',
               controllerAs: 'vm'
           })
           .when('/shapes/time-trial', {
-              templateUrl: 'scripts/preattentive_shape/time_trial/view.html',
+              templateUrl: 'views/preattentive_shape/time_trial.html',
               controller: 'CtrlShapeTimeTrial',
               controllerAs: 'vm'
           })
           .when('/shapes/short-visible', {
-              templateUrl: 'scripts/preattentive_shape/short_visible/view.html',
+              templateUrl: 'views/preattentive_shape/short_visible.html',
               controller: 'CtrlShapeShortVisible',
               controllerAs: 'vm'
           })
           .when('/conjunction/time-trial', {
-              templateUrl: 'scripts/preattentive_conjunction/time_trial/view.html',
+              templateUrl: 'views/preattentive_conjunction/time_trial.html',
               controller: 'CtrlConjunctionTimeTrial',
               controllerAs: 'vm'
           })
           .when('/conjunction/short-visible', {
-              templateUrl: 'scripts/preattentive_conjunction/short_visible/view.html',
+              templateUrl: 'views/preattentive_conjunction/short_visible.html',
               controller: 'CtrlConjunctionShortVisible',
               controllerAs: 'vm'
           })
--- app/scripts/preattentive_conjunction/CtrlPreConjunction.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('infoVisU2App')
-    .controller('CtrlPreConjunction', CtrlPreConjunction);
-
-CtrlPreConjunction.$inject = [];
-function CtrlPreConjunction(){
-
-}
